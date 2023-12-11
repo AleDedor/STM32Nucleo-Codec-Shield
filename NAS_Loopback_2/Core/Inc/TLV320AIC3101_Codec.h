5df2f547ab3751409d24a293e55805322572a28c
Luca452 <daidoneluca98@gmail.com>
2023-12-11 10:36:16 +0100

    Added new loopback test project

---
+++ NAS_Loopback_2/Core/Inc/TLV320AIC3101_Codec.h
@@ -0,0 +1,18 @@
+#ifndef SRC_TLV320AIC3101_CODEC_H_
+#define SRC_TLV320AIC3101_CODEC_H_
+
+#define CODEC_I2C_ADDR 0b00110000
+#define I2C_MEMADD_SIZE 1
+#define RESET_TIME 50
+
+//magari si può fare il define dei led qua
+// e aggiungere i led nello struct che definisce il codec
+typedef struct{
+	I2C_HandleTypeDef *I2Chandle;
+}Codec;
+
+HAL_StatusTypeDef Codec_Init(Codec *codec, I2C_HandleTypeDef *I2Chandle);
+HAL_StatusTypeDef Codec_WriteRegister(Codec *codec, uint8_t *reg_add_val, uint8_t len_array_reg);
+HAL_StatusTypeDef Codec_ReadRegister(Codec *codec, uint8_t reg_addr, uint8_t *val);
+
+#endif
